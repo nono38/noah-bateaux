4ef74ac789755fda7bb080289dfb8ba94f670ffd
nono38 <74985866+nono38@users.noreply.github.com>
2021-05-18 17:19:28 +0200

    Fichiers initiaux pour un projet MakeCode

---
+++ main.ts
@@ -0,0 +1,15 @@
+radio.onReceivedValue(function (name, value) {
+    if (name == "p_1" && value == 0) {
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+        kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
+    } else if (name == "p_1" && value > 0) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Reverse, value)
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Forward, value)
+    } else if (name == "p_1" && value <= -1) {
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor2, kitronik_motor_driver.MotorDirection.Forward, Math.abs(value))
+        kitronik_motor_driver.motorOn(kitronik_motor_driver.Motors.Motor1, kitronik_motor_driver.MotorDirection.Reverse, Math.abs(value))
+    }
+})
+radio.setGroup(197)
+kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor2)
+kitronik_motor_driver.motorOff(kitronik_motor_driver.Motors.Motor1)
